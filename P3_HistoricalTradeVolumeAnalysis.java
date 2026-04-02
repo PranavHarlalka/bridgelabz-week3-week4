0252782d71309fd35722fd7c8ccc08fabc731ecf
PranavHarlalka <pranavharlalka2@gmail.com>
2026-04-02 14:15:09 +0530

    Add week 3 solutions

---
+++ P3_HistoricalTradeVolumeAnalysis.java
@@ -0,0 +1,91 @@
+class Trade {
+    String id;
+    int volume;
+
+    public Trade(String id, int volume) {
+        this.id = id;
+        this.volume = volume;
+    }
+}
+
+public class P3_HistoricalTradeVolumeAnalysis {
+
+    public static void mergeSort(Trade[] arr, int left, int right) {
+        if (left < right) {
+            int mid = left + (right - left) / 2;
+            mergeSort(arr, left, mid);
+            mergeSort(arr, mid + 1, right);
+            merge(arr, left, mid, right);
+        }
+    }
+
+    private static void merge(Trade[] arr, int left, int mid, int right) {
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
+        Trade[] L = new Trade[n1];
+        Trade[] R = new Trade[n2];
+
+        System.arraycopy(arr, left, L, 0, n1);
+        System.arraycopy(arr, mid + 1, R, 0, n2);
+
+        int i = 0, j = 0, k = left;
+        while (i < n1 && j < n2) {
+            if (L[i].volume <= R[j].volume) {
+                arr[k] = L[i];
+                i++;
+            } else {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+        while (i < n1) { arr[k] = L[i]; i++; k++; }
+        while (j < n2) { arr[k] = R[j]; j++; k++; }
+    }
+
+    public static void quickSortDesc(Trade[] arr, int low, int high) {
+        if (low < high) {
+            int pi = partition(arr, low, high);
+            quickSortDesc(arr, low, pi - 1);
+            quickSortDesc(arr, pi + 1, high);
+        }
+    }
+
+    private static int partition(Trade[] arr, int low, int high) {
+        int pivot = arr[high].volume;
+        int i = (low - 1);
+        for (int j = low; j < high; j++) {
+            if (arr[j].volume >= pivot) {
+                i++;
+                Trade temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        Trade temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+        return i + 1;
+    }
+
+    public static Trade[] mergeSortedLists(Trade[] list1, Trade[] list2) {
+        Trade[] merged = new Trade[list1.length + list2.length];
+        int i = 0, j = 0, k = 0;
+        while (i < list1.length && j < list2.length) {
+            if (list1[i].volume <= list2[j].volume) {
+                merged[k++] = list1[i++];
+            } else {
+                merged[k++] = list2[j++];
+            }
+        }
+        while (i < list1.length) merged[k++] = list1[i++];
+        while (j < list2.length) merged[k++] = list2[j++];
+        return merged;
+    }
+
+    public static long computeTotalVolume(Trade[] arr) {
+        long total = 0;
+        for (Trade t : arr) total += t.volume;
+        return total;
+    }
+}
